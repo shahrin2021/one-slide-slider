e164014bba5e8ace7942e0e8404e1eeeb63d7855
Md Shahrin Bin Selim <mdshahrinbinselim1991@gmail.com>
2021-11-30 22:10:52 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+
+import "./styles.css";
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Navigation,Pagination,History
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Navigation,Pagination,History]);
+
+
+export default function App() {
+  
+  
+  
+  return (
+    <>
+    <Swiper spaceBetween={50} slidesPerView={1} navigation={true} pagination={true} history={{
+  "key": "slide"
+}} className="mySwiper">
+  <SwiperSlide data-history="1">Slide 1</SwiperSlide><SwiperSlide data-history="Slide 2">Slide 2</SwiperSlide><SwiperSlide data-history="3">Slide 3</SwiperSlide><SwiperSlide data-history="Slide 4">Slide 4</SwiperSlide><SwiperSlide data-history="5">Slide 5</SwiperSlide><SwiperSlide data-history="Slide 6">Slide 6</SwiperSlide><SwiperSlide data-history="7">Slide 7</SwiperSlide><SwiperSlide data-history="Slide 8">Slide 8</SwiperSlide><SwiperSlide data-history="9">Slide 9</SwiperSlide>
+  </Swiper>
+    </>
+  )
+}
